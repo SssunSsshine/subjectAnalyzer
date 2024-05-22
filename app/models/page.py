c99de9534358b442927301979c31f50c309517d1
Alexandra Nasirkulova <alexn10603@gmail.com>
2024-05-22 12:07:19 +0300

    Page table fixed by adding unique constraint for url.

--- app/models/page.py
+++ app/models/page.py
@@ -6,6 +6,6 @@ class Page(db.Model):
 
     id = db.Column(db.BigInteger, primary_key=True)
     webside_id = db.Column(db.BigInteger, db.ForeignKey('website.id', ondelete='CASCADE'), nullable=False)
-    url = db.Column(db.String(255), nullable=False)
+    url = db.Column(db.String(255), nullable=False, unique=True)
     content = db.Column(db.Text, nullable=False)
     crawled_at = db.Column(db.DateTime)
